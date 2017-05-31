1ea3ac5f0b942d4c7ea60c4de048367bd518ec6b
jemison <jemison321@gmail.com>
2017-05-30 21:05:13 -0300

    Create QRCode reader and activity to show result

---
+++ app/src/main/java/com/example/james/softmakrqrcode/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.james.softmakrqrcode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scannerView = new ZXingScannerView(this);
+        scannerView.setResultHandler(new ZXingScannerResultHandler());
+
+        setContentView(scannerView);
+        scannerView.startCamera();
+    }
+
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    class ZXingScannerResultHandler implements ZXingScannerView.ResultHandler{
+
+        public final static String EXTRA_MESSAGE = "com.example.james.softmarkqrcode";
+
+        @Override
+        public void handleResult(Result result){
+            String resultCode = result.getText();
+            setContentView(R.layout.activity_main);
+            scannerView.stopCamera();
+
+
+            Intent intent = new Intent(MainActivity.this, DisplayCoupon.class);
+            intent.putExtra(EXTRA_MESSAGE, resultCode);
+            startActivity(intent);
+        }
+    }
+
+}
